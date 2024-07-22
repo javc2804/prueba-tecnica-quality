84bd7a30228fc125252edbca3e1d7db630f5729d
javier <javc2804@gmail.com>
2024-07-22 19:02:38 -0400

    estructura base

---
+++ src/app/dashboard/button/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const page = () => {
+  return <Button>Button</Button>;
+};
+
+export default page;
---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+const links = [
+  { name: "button", href: "button" },
+  { name: "accordion", href: "accordion" },
+];
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  return (
+    <>
+      <nav className="bg-white border-b border-gray-200 fixed z-30 w-full">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start">
+              <button
+                id="toggleSidebarMobile"
+                aria-expanded={isSidebarOpen ? "true" : "false"}
+                aria-controls="sidebar"
+                className="lg:hidden mr-2 text-gray-600 hover:text-gray-900 cursor-pointer p-2 hover:bg-gray-100 focus:bg-gray-100 focus:ring-2 focus:ring-gray-100 rounded"
+                onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              >
+                <svg
+                  id="toggleSidebarMobileHamburger"
+                  className={`w-6 h-6 ${isSidebarOpen ? "hidden" : "block"}`}
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h6a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  id="toggleSidebarMobileClose"
+                  className={`w-6 h-6 ${isSidebarOpen ? "block" : "hidden"}`}
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <a
+                href="#"
+                className="text-xl font-bold flex items-center lg:ml-2.5"
+              >
+                <span className="self-center whitespace-nowrap ml-2">
+                  Rick And Morty
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="flex overflow-hidden bg-white pt-16">
+        <aside
+          id="sidebar"
+          className={`fixed z-20 h-full top-0 left-0 pt-16 lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75 ${
+            isSidebarOpen ? "block" : "hidden"
+          } lg:block`}
+          aria-label="Sidebar"
+        >
+          <div className="relative flex-1 flex flex-col min-h-0 borderR border-gray-200 bg-white pt-0">
+            <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+              <div className="flex-1 px-3 bg-white divide-y space-y-1">
+                <ul className="space-y-2 pb-2">
+                  {links.map((link) => (
+                    <li key={link.href}>
+                      <Link
+                        href={link.href}
+                        className="text-base capitalize text-gray-900 font-normal rounded-lg flex items-center p-2 hover:bg-gray-100 group"
+                      >
+                        <span className="ml-3">{link.name}</span>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </aside>
+        <div
+          className="bg-gray-900 opacity-50 hidden fixed inset-0 z-10"
+          id="sidebarBackdrop"
+        ></div>
+        <div
+          id="main-content"
+          className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64"
+        >
+          <main>
+            <div className="pt-6 px-4">
+              <div className="w-full min-h-[calc(100vh-230px)]">
+                <div className="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </main>
+          <p className="text-center text-sm text-gray-500 my-10">
+            &copy; {new Date().getFullYear()}{" "}
+            <a href="#" className="hover:underline" target="_blank">
+              Rick and Morty
+            </a>
+            . All rights reserved.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
