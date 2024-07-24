7221db0707474ac643505facbedf082b3e16ecde
javier <javc2804@gmail.com>
2024-07-24 12:32:33 -0400

    componente editar y crear

--- src/app/dashboard/muestra/page.tsx
+++ src/app/dashboard/muestra/page.tsx
@@ -31,14 +31,18 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div>
-      <AddCharacterForm />
-      <CharactersTable apiCharacters={apiCharacters} />
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={handlePageChange}
-      />
+    <div className="flex flex-col md:flex-row">
+      <div className="md:w-2/5 w-full">
+        <AddCharacterForm />
+      </div>
+      <div className="md:w-3/5 w-full">
+        <CharactersTable apiCharacters={apiCharacters} />
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 };
--- src/components/AddCharacterForm/AddCharacterForm.tsx
+++ src/components/AddCharacterForm/AddCharacterForm.tsx
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useCharacterStore } from "../../stores/store";
-import { Character } from "../../types/types";
 
 const AddCharacterForm: React.FC = () => {
   const editingCharacter = useCharacterStore((state) => state.editingCharacter);
   const [name, setName] = useState("");
   const [status, setStatus] = useState("");
-  const [species, setSpecies] = useState(""); // Nuevo estado para especie
-  const [type, setType] = useState(""); // Nuevo estado para tipo
+  const [species, setSpecies] = useState("");
+  const [type, setType] = useState("");
   const addCharacter = useCharacterStore((state) => state.addCharacter);
   const updateCharacter = useCharacterStore((state) => state.updateCharacter);
   const setEditingCharacter = useCharacterStore(
@@ -18,8 +17,8 @@ const AddCharacterForm: React.FC = () => {
     if (editingCharacter) {
       setName(editingCharacter.name);
       setStatus(editingCharacter.status);
-      setSpecies(editingCharacter.species || ""); // Ajuste para especie
-      setType(editingCharacter.type || ""); // Ajuste para tipo
+      setSpecies(editingCharacter.species || "");
+      setType(editingCharacter.type || "");
     }
   }, [editingCharacter]);
 
@@ -39,38 +38,50 @@ const AddCharacterForm: React.FC = () => {
     }
     setName("");
     setStatus("");
-    setSpecies(""); // Limpiar estado de especie
-    setType(""); // Limpiar estado de tipo
+    setSpecies("");
+    setType("");
     setEditingCharacter(null);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4">
+      <h1 className="text-center text-2xl font-bold my-4">
+        {editingCharacter ? "Editar Personaje" : "Crear Personaje"}
+      </h1>{" "}
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
-        placeholder="Character Name"
+        placeholder="Nombre"
+        className="border border-gray-300 p-2 rounded-md"
       />
       <input
         type="text"
         value={status}
         onChange={(e) => setStatus(e.target.value)}
         placeholder="Estado"
+        className="border border-gray-300 p-2 rounded-md"
       />
       <input
         type="text"
         value={species}
         onChange={(e) => setSpecies(e.target.value)}
         placeholder="Especie"
+        className="border border-gray-300 p-2 rounded-md"
       />
       <input
         type="text"
         value={type}
         onChange={(e) => setType(e.target.value)}
         placeholder="Tipo"
+        className="border border-gray-300 p-2 rounded-md"
       />
-      <button type="submit">Save Character</button>
+      <button
+        type="submit"
+        className="bg-green-500 text-white p-2 rounded-md hover:bg-green-600"
+      >
+        Guardar
+      </button>
     </form>
   );
 };
