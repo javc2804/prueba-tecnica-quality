baebd95da00ca81a1ca38592da2d6d9cd2db8952
javier <javc2804@gmail.com>
2024-07-24 13:07:47 -0400

    ajustes

--- src/components/CharactersTable/CharactersTable.tsx
+++ src/components/CharactersTable/CharactersTable.tsx
@@ -14,24 +14,18 @@ import {
 } from "@/components/ui/table";
 import { Button } from "../ui/button";
 
-const invoices = [];
-
 interface Props {
   apiCharacters: Character[];
 }
 
 const CharactersTable: React.FC<Props> = ({ apiCharacters }) => {
-  const {
-    characters: localCharacters,
-    updateCharacter,
-    editingCharacter,
-    setEditingCharacter,
-  } = useCharacterStore((state) => ({
-    characters: state.characters,
-    updateCharacter: state.updateCharacter,
-    editingCharacter: state.editingCharacter,
-    setEditingCharacter: state.setEditingCharacter,
-  }));
+  const { characters: localCharacters, setEditingCharacter } =
+    useCharacterStore((state) => ({
+      characters: state.characters,
+      updateCharacter: state.updateCharacter,
+      editingCharacter: state.editingCharacter,
+      setEditingCharacter: state.setEditingCharacter,
+    }));
 
   const [currentPage, setCurrentPage] = useState(1);
   const charactersPerPage = 10;
@@ -59,84 +53,42 @@ const CharactersTable: React.FC<Props> = ({ apiCharacters }) => {
     setEditingCharacter(character);
   };
 
-  const saveEdits = () => {
-    if (editingCharacter) {
-      updateCharacter(editingCharacter);
-      setEditingCharacter(null);
-    }
-  };
-
-  const handleEditChange = (
-    e: React.ChangeEvent<HTMLInputElement>,
-    field: keyof Character
-  ) => {
-    if (editingCharacter) {
-      setEditingCharacter({ ...editingCharacter, [field]: e.target.value });
-    }
-  };
-
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">id</TableHead>
-          <TableHead>Nombre</TableHead>
-          <TableHead>Estado</TableHead>
-          <TableHead>Especie</TableHead>
-          <TableHead>Tipo</TableHead>
-          <TableHead>Acciones</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {currentCharacters.map((character) => (
-          <TableRow key={character.id}>
-            <TableCell>{character.id}</TableCell>
-            <TableCell>{character.name}</TableCell>
-            <TableCell>{character.status}</TableCell>
-            <TableCell>{character.species}</TableCell>
-            <TableCell>{character.type}</TableCell>
-            <TableCell>
-              <Button onClick={() => startEditing(character)}>Edit</Button>
-            </TableCell>
+    <>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">id</TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Estado</TableHead>
+            <TableHead>Especie</TableHead>
+            <TableHead>Tipo</TableHead>
+            <TableHead>Acciones</TableHead>
           </TableRow>
+        </TableHeader>
+        <TableBody>
+          {currentCharacters.map((character) => (
+            <TableRow key={character.id}>
+              <TableCell>{character.id}</TableCell>
+              <TableCell>{character.name}</TableCell>
+              <TableCell>{character.status}</TableCell>
+              <TableCell>{character.species}</TableCell>
+              <TableCell>{character.type}</TableCell>
+              <TableCell>
+                <Button onClick={() => startEditing(character)}>Edit</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button key={index} onClick={() => setCurrentPage(index + 1)}>
+            {index + 1}
+          </button>
         ))}
-      </TableBody>
-    </Table>
-    // <div>
-    //   <table>
-    //     <thead>
-    //       <tr>
-    //         <th>ID</th>
-    //         <th>Name</th>
-    //         <th>Estado</th>
-    //         <th>Especie</th>
-    //         <th>Tipo</th>
-    //         <th>Actions</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       {currentCharacters.map((character) => (
-    //         <tr key={character.id}>
-    //           <td>{character.id}</td>
-    //           <td>{character.name}</td>
-    //           <td>{character.status}</td>
-    //           <td>{character.species}</td>
-    //           <td>{character.type}</td>
-    //           <td>
-    //             <button onClick={() => startEditing(character)}>Edit</button>
-    //           </td>
-    //         </tr>
-    //       ))}
-    //     </tbody>
-    //   </table>
-    //   <div>
-    //     {Array.from({ length: totalPages }, (_, index) => (
-    //       <button key={index} onClick={() => setCurrentPage(index + 1)}>
-    //         {index + 1}
-    //       </button>
-    //     ))}
-    //   </div>
-    // </div>
+      </div>
+    </>
   );
 };
 
